ddfc36242f3ac3e2cb6409ee61e7c360b0fec35e
injunSeo <fregesaxiom@gmail.com>
2021-12-02 19:15:21 +0900

    generation 수정

--- src/main/java/jpaProject/trouble_Solution/domain/GenerationStatus.java
+++ src/main/java/jpaProject/trouble_Solution/domain/GenerationStatus.java
@@ -1,7 +1,7 @@
 package jpaProject.trouble_Solution.domain;
 
 public enum GenerationStatus {
-    TEEN("십대"), TWENTYToTHIRTY("20-30대"), ELDER("40-50대");
+    TEEN("10대"), TWENTYToTHIRTY("20-30대"), ELDER("40-50대");
 
     private final String description;
 
