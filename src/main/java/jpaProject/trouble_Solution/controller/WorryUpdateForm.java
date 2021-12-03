bc05e9085427140b52aeeb671689a03bda1da348
injunSeo <fregesaxiom@gmail.com>
2021-12-03 23:04:33 +0900

    UpdateWorry

--- src/main/java/jpaProject/trouble_Solution/controller/WorryUpdateForm.java
+++ src/main/java/jpaProject/trouble_Solution/controller/WorryUpdateForm.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter @Setter
-public class WorrySaveForm {
+public class WorryUpdateForm {
     private Long id;
 
     private Member member;
