ca6208bc4ae13ab667944e2b9a6dc9fb043d4d24
injunSeo <fregesaxiom@gmail.com>
2021-12-06 23:44:58 +0900

    add desc to 'READY' and 'SOLVED'

--- src/main/java/jpaProject/trouble_Solution/domain/SolvedStatus.java
+++ src/main/java/jpaProject/trouble_Solution/domain/SolvedStatus.java
@@ -1,5 +1,16 @@
 package jpaProject.trouble_Solution.domain;
 
 public enum SolvedStatus {
-    READY, SOLVED
+    READY("미해결"), SOLVED("해결 완료");
+
+    private final String description;
+
+
+    SolvedStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
