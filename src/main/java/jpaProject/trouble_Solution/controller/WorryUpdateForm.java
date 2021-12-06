e9301e80729d09dd90448be4f9449c8741ada7c5
injunSeo <fregesaxiom@gmail.com>
2021-12-06 23:43:09 +0900

    add variable SolvedStatus

--- src/main/java/jpaProject/trouble_Solution/controller/WorryUpdateForm.java
+++ src/main/java/jpaProject/trouble_Solution/controller/WorryUpdateForm.java
@@ -2,6 +2,7 @@ package jpaProject.trouble_Solution.controller;
 
 import jpaProject.trouble_Solution.domain.GenerationStatus;
 import jpaProject.trouble_Solution.domain.Member;
+import jpaProject.trouble_Solution.domain.SolvedStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Getter @Setter
 public class WorryUpdateForm {
+
     private Long id;
 
     private Member member;
@@ -23,11 +25,14 @@ public class WorryUpdateForm {
     private String content;
 
     private LocalDateTime createDate;
+
     @NotNull(message = "세대 선택은 필수입니다.")
     private GenerationStatus generation;
 
     @NotEmpty(message = "카테고리를 선택해주세요.")
     private List<Long> categoryId;
+
+    private SolvedStatus solvedStatus;
 
 
 }
