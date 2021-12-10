e38546a740f82cc022e49bfecf76ed4fd4ce2d4a
injunSeo <fregesaxiom@gmail.com>
2021-12-10 17:34:08 +0900

    Form for Solution save

--- src/main/java/jpaProject/trouble_Solution/controller/SolutionSaveForm.java
+++ src/main/java/jpaProject/trouble_Solution/controller/SolutionSaveForm.java
@@ -1,2 +1,24 @@
-package jpaProject.trouble_Solution.controller;public class SolutionSaveForm {
+package jpaProject.trouble_Solution.controller;
+
+import jpaProject.trouble_Solution.domain.Member;
+import jpaProject.trouble_Solution.domain.Worry;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+public class SolutionSaveForm {
+
+    private Long id;
+
+    private Worry worry;
+
+    private Member member;
+
+    @NotEmpty(message = "content는 필수입니다")
+    private String content;
 }
